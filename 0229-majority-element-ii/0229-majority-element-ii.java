6796468c89795857d5d2b7a05d27a6edfe752071
Rishav Verma <166117840+Rishav345@users.noreply.github.com>
2026-09-09 11:36:12 +0530

    Time: 3 ms (73.43%), Space: 52.3 MB (74.77%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int elm1 = Integer.MIN_VALUE;
+        int elm2 = Integer.MIN_VALUE;
         int n = nums.length;
         for (int i = 0; i < n; i++) {
-            int count = 1;
-            for (int j = i+1; j < n; j++) {
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
-            if(count > n/3 && !ans.contains(nums[i])){
-                ans.add(nums[i]);
+            if (nums[i] != elm2 && cnt1 == 0) {
+                elm1 = nums[i];
+                cnt1 = 1;
+            } else if (nums[i] != elm1 && cnt2 == 0) {
+                elm2 = nums[i];
+                cnt2 = 1;
+            } else if (elm1 == nums[i]) {
+                cnt1++;
+            } else if (elm2 == nums[i]) {
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
             }
         }
-        return ans;
+        cnt1 = 0; cnt2 = 0; 
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == elm1) cnt1++; 
+            if (nums[i] == elm2) cnt2++;
+        }
+
+        if (cnt1 > n / 3 && cnt2 > n / 3) {
+            return List.of(elm1, elm2);
+        }
+        if (cnt1 > n / 3) {
+            return List.of(elm1);
+        }
+        if (cnt2 > n / 3) {
+            return List.of(elm2);
+        }
+
+        return List.of();
     }
 }
